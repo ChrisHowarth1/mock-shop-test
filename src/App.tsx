e6f7bc4786fceee3f0b3d095183e481023f61956
Chris Howarth <c.a.howarth@live.co.uk>
2025-03-13 12:42:43 +0000

    aligning component casing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { ProductGrid } from './components/ProductGrid';
+import { ProductGrid } from './components/productGrid';
 import { Accordion } from './components/accordion';
 import { Header } from './components/header';
 
